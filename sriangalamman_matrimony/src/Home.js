53e1b402909a5bc9ca46fbf4ead14e626bc98537
mg-dev04 <mg.debugger@gmail.com>
2025-09-16 16:20:05 +0530

    header_initial

--- sriangalamman_matrimony/src/Home.js
+++ sriangalamman_matrimony/src/Home.js
@@ -1,5 +1,22 @@
+import React from "react";
+import './Home.css';
+import logo from './logo.png';
+
 function Home() {
-    <div className="header"></div>
-     
+    return(
+    <div className="header">
+        <div className="logo">
+            <img src={logo} alt="logo"/>
+        </div>
+        <div className="headercontact">
+            <h3>+919443946541</h3>
+            <h2>info@sriangalammanmatrimony.com</h2>
+        </div>
+        <div className="headerbuttons">
+            <button>Login</button>
+            <button>Register</button>
+        </div>
+    </div>
+    )
 }
 export default Home;
